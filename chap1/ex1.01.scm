275cc469ab6f618d09f92f6cbd9598094b7bf8df
Yuto Tagashira <gh0st1nth3sh311.dev@gmail.com>
2018-03-31 15:44:13 +0900

    answer ex1.01

--- chap1/ex1.01.scm
+++ chap1/ex1.01.scm
@@ -1,2 +1,25 @@
 10
 (+ 5 3 4)
+(- 9 1)
+(/ 6 2)
+(+ (* 2 4) (- 4 6))
+(define a 3)
+(define b (+ a 1))
+(newline)(display
+(+ a b (* a b))
+)(newline)(display
+(if (and (> b a) (< b (* a b)))
+    b
+    a)
+)(newline)(display
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
+)(newline)(display
+(+ 2 (if (> b a) b a))
+)(newline)(display
+(* (cond ((> a b) a)
+	 ((< a b) b)
+	 (else -1))
+   (+ a 1))
+)
